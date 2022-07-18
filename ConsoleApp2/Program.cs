c93be452cb0990a635fd4df16ed55482a7cd5f2e
PavelSGitHub <pawa1337@gmail.com>
2022-07-18 19:18:02 +0700

    solved kata https://www.codewars.com/kata/56a3f08aa9a6cc9b75000023

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -11,7 +11,7 @@ namespace ConsoleApp2
             //Robot vicky = new Robot();
             //Console.WriteLine(TwoSum(new[] { 1234, 5678, 9012 }, 14690)[0] + " " + TwoSum(new[] { 1234, 5678, 9012 }, 14690)[1]);
 
-            Console.WriteLine(RegexpBasics.Digit("4"));
+            Console.WriteLine(SimpleValidation.ValidateUsr("asddsa"));
    
         }
 
---
+++ ConsoleApp2/SimpleValidation.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+
+//https://www.codewars.com/kata/56a3f08aa9a6cc9b75000023
+namespace ConsoleApp2
+{
+    internal class SimpleValidation
+    {
+        public static bool ValidateUsr(string username)
+        {
+            return Regex.IsMatch(username, @"^[a-z\d_]{4,16}\z");
+        }
+    }
+}
