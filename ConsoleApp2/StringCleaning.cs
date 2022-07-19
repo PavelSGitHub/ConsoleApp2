85aa77cc6f8c0c1d0606bb98d39f764cb80f1045
PavelSGitHub <pawa1337@gmail.com>
2022-07-19 15:12:12 +0700

    solved kata https://www.codewars.com/kata/57e1e61ba396b3727c000251

---
+++ ConsoleApp2/StringCleaning.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+
+namespace ConsoleApp2
+{
+    internal class StringCleaning
+    {
+        public static string StringClean(string s)
+        {
+            return string.Concat(Regex.Matches(s, @"[~#$%^&!@*():;'.,?`\u0022a-zA-Z\s]*"));
+        }
+    }
+}
