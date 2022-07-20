7591e904a5694fbcc3eff61cdf422fc7cb68d8a3
PavelSGitHub <pawa1337@gmail.com>
2022-07-20 16:28:36 +0700

    solved kata https://www.codewars.com/kata/585b1fafe08bae9988000314

---
+++ ConsoleApp2/Digits explosion.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+
+//https://www.codewars.com/kata/585b1fafe08bae9988000314
+namespace ConsoleApp2
+{
+    internal class DigitsExplosion
+    {
+        public static string Explode(string s)
+        {
+            return string.Concat(Regex.Matches(s, @"\d").Select(match => new string(char.Parse(match.Value), int.Parse(match.Value))));
+        }
+    }
+}
