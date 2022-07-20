f22ad21e32c19a6f0b91f8bc28d707ed1fa57880
PavelSGitHub <pawa1337@gmail.com>
2022-07-20 15:12:43 +0700

    solved kata https://www.codewars.com/kata/57a37f3cbb99449513000cd8

---
+++ ConsoleApp2/GetNumberFromString.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+
+// https://www.codewars.com/kata/57a37f3cbb99449513000cd8
+namespace ConsoleApp2
+{
+    internal class GetNumberFromString
+    {
+        public static int getNumberFromString(string s)
+        {
+            return int.Parse(string.Concat(Regex.Matches(s, @"\d")));
+        }
+    }
+}
