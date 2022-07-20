448c7653641a636efc421243851ec46425034c49
PavelSGitHub <pawa1337@gmail.com>
2022-07-20 15:25:58 +0700

    solved kata https://www.codewars.com/kata/56a946cd7bd95ccab2000055

---
+++ ConsoleApp2/RegexCountLowercaseLetters.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+
+//https://www.codewars.com/kata/56a946cd7bd95ccab2000055
+namespace ConsoleApp2
+{
+    internal class RegexCountLowercaseLetters
+    {
+        public static int LowercaseCountCheck(string s)
+        {
+            return Regex.Matches(s, "[a-z]").Count;
+        }
+    }
+}
