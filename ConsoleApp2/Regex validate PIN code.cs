f7a5a2c3befb867e82659dfd964d5b09a83934ed
PavelSGitHub <pawa1337@gmail.com>
2022-07-20 16:08:43 +0700

    solved kata https://www.codewars.com/kata/55f8a9c06c018a0d6e000132

---
+++ ConsoleApp2/Regex validate PIN code.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+
+//https://www.codewars.com/kata/55f8a9c06c018a0d6e000132
+namespace ConsoleApp2
+{
+    internal class RegexValidatePINCode
+    {
+        public static bool ValidatePin(string pin)
+        {
+            return Regex.IsMatch(pin, @"^[0-9]{4}\z|^[0-9]{6}\z");
+        }
+    }
+}
