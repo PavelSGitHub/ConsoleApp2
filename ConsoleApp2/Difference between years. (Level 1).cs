c05cbc88f5aca5ebc8558fecb1dbe2424aa4145d
PavelSGitHub <pawa1337@gmail.com>
2022-07-22 15:17:50 +0700

    kata solved https://www.codewars.com/kata/588f5a38ec641b411200005b

---
+++ ConsoleApp2/Difference between years. (Level 1).cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+
+//https://www.codewars.com/kata/588f5a38ec641b411200005b
+namespace ConsoleApp2
+{
+    internal class DifferenceBetweenYears
+    {
+        public static int HowManyYears(string date1, string date2)
+        {
+            return Math.Abs(int.Parse(Regex.Match(date1, @"\d*").Value) - int.Parse(Regex.Match(date2, @"\d*").Value));
+        }
+    }
+}
