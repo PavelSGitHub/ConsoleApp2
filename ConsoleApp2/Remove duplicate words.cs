ee03e8f069eb0caf0c795ec78a118dc184ccdd7c
PavelSGitHub <pawa1337@gmail.com>
2022-07-22 17:21:55 +0700

    solved kata https://www.codewars.com/kata/5b39e3772ae7545f650000fc

---
+++ ConsoleApp2/Remove duplicate words.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+
+//https://www.codewars.com/kata/5b39e3772ae7545f650000fc
+namespace ConsoleApp2
+{
+    internal class Remove_duplicate_words
+    {
+        public static string RemoveDuplicateWords(string s)
+        {
+            return string.Join(' ', Regex.Matches(s, @"\S+").DistinctBy(match => match.Value));
+  }
+    }
+}
