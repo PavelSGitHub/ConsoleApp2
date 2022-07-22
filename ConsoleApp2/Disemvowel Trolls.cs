77f9dce1e246adf9d033f35c137d3edae442e0e0
PavelSGitHub <pawa1337@gmail.com>
2022-07-22 15:24:05 +0700

    solved kata https://www.codewars.com/kata/52fba66badcd10859f00097e

---
+++ ConsoleApp2/Disemvowel Trolls.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+
+//https://www.codewars.com/kata/52fba66badcd10859f00097e
+namespace ConsoleApp2
+{
+    internal class Disemvowel_Trolls
+    {
+        public static string Disemvowel(string str)
+        {
+            return Regex.Replace(str, @"[euioaEUIOA]", string.Empty);
+        }
+    }
+}
