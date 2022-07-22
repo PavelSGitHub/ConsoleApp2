3b24887531afccee8b81e8d6fc8c779b7bf79deb
PavelSGitHub <pawa1337@gmail.com>
2022-07-22 16:30:54 +0700

    solved kata https://www.codewars.com/kata/5b37a50642b27ebf2e000010

---
+++ ConsoleApp2/Sum of a Beach.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+
+//https://www.codewars.com/kata/5b37a50642b27ebf2e000010
+namespace ConsoleApp2
+{
+    internal class Sum_of_a_Beach
+    {
+        public static int SumOfABeach(string s)
+        {
+            return Regex.Matches(s, @"sand|water|fish|sun", RegexOptions.IgnoreCase).Count;
+        }
+    }
+}
