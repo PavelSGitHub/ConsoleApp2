7d334a8799db77d516705e88fae30c353fea71ff
PavelSGitHub <pawa1337@gmail.com>
2022-07-22 16:23:41 +0700

    solved kata https://www.codewars.com/kata/5639bdcef2f9b06ce800005b

---
+++ ConsoleApp2/Return String of First Characters.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+
+//https://www.codewars.com/kata/5639bdcef2f9b06ce800005b
+namespace ConsoleApp2
+{
+    internal class Return_String_of_First_Characters
+    {
+        public static string MakeString(string s)
+        {
+            return string.Concat(Regex.Matches(s, @"\b[a-zA-Z]"));
+        }
+    }
+}
