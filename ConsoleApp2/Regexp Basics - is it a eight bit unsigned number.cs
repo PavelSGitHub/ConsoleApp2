b274ce335203966cfd089d615e6c0117bec03254
PavelSGitHub <pawa1337@gmail.com>
2022-07-25 21:20:18 +0700

    solved kata https://www.codewars.com/kata/567e8f7b4096f2b4b1000005

---
+++ ConsoleApp2/Regexp Basics - is it a eight bit unsigned number.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+
+//https://www.codewars.com/kata/567e8f7b4096f2b4b1000005
+namespace ConsoleApp2
+{
+    static class Regexp_Basics___is_it_a_eight_bit_unsigned_number
+    {
+        public static bool EightBitNumber(this string str)
+        {
+            foreach (var item in str)
+            {
+                Console.WriteLine((int)item);
+            }
+            return Regex.IsMatch(str, @"^[0-9]{1}$|^[1-9]{1}[0-9]{1}$|^1[0-9]{1,2}$|^2[0-4]{1}[0-9]{1}$|^2[0-5]{1,2}\z");
+        }
+    }
+}
