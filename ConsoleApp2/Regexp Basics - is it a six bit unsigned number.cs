ecc76d8eb81d0b7cf05e36839ebd74543635b2a4
PavelSGitHub <pawa1337@gmail.com>
2022-07-25 21:24:59 +0700

    solved kata https://www.codewars.com/kata/567e8dbb9b6f4da558000030

---
+++ ConsoleApp2/Regexp Basics - is it a six bit unsigned number.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+
+//https://www.codewars.com/kata/567e8dbb9b6f4da558000030
+namespace ConsoleApp2
+{
+    static internal class Regexp_Basics___is_it_a_six_bit_unsigned_number
+    {
+        public static bool SixBitNumber(this string str)
+        {
+            return Regex.IsMatch(str, @"^([0-9]{1}|[1-5]{1}[0-9]{1}|6[0-3]{1})\z");
+        }
+    }
+}
