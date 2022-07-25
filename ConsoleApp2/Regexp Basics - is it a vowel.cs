808dda35035e41698d19bf9a332c660b28aa64d2
PavelSGitHub <pawa1337@gmail.com>
2022-07-25 21:30:54 +0700

    solved kata https://www.codewars.com/kata/567bed99ee3451292c000025

---
+++ ConsoleApp2/Regexp Basics - is it a vowel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+
+//https://www.codewars.com/kata/567bed99ee3451292c000025
+namespace ConsoleApp2
+{
+    static internal class Regexp_Basics___is_it_a_vowel
+    {
+        public static bool Vowel(this string s)
+        {
+            return Regex.IsMatch(s, @"^(a|e|u|i|o)\z", RegexOptions.IgnoreCase);
+        }
+    }
+}
